e4a26173c8f06d3f52d7fbe5cfdf83742d23a110
Izabela <izabela.nowosada@gmail.com>
2019-01-12 08:12:15 +0100

    add hero object and basic game logic

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,35 @@
 // Write your JS here
+let hero = {
+    name: 'Superman',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'sword',
+        damage: 2
+    },
+};
+
+const dagger = {
+    type: 'dagger',
+    damage: 2
+};
+
+function rest(object) {
+    object.health = 10;
+    return object;
+};
+
+function pickUpItem(whoPicks, pickedWeapon) {
+    whoPicks.inventory.push(pickedWeapon);
+};
+
+function equipWeapon(whomEquip) {
+    if (!whomEquip.weapon && whomEquip.inventory.lenght > 0) {
+        whomEquip.weapon.type = whomEquip.inventory[0].type;
+        whomEquip.weapon.damage += whomEquip.inventory[0].damage;
+    }
+    console.log(whomEquip.inventory);
+    console.log(whomEquip.weapon);
+};
+
