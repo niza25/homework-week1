e79c38ccfaee1704c0b75a3c63b1b765f181301a
Izabela <izabela.nowosada@gmail.com>
2019-01-12 11:46:14 +0100

    add bonus functionality and style

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -10,44 +10,85 @@ let hero = {
     },
 };
 
+let hero2 = {
+    name: 'Superwoman',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'pistol',
+        damage: 3
+    },
+};
+
 const dagger = {
     type: 'dagger',
     damage: 2
 };
 
+const changeNameForm = document.getElementById('changeNameForm');
+const container = document.getElementById('container');
+const statsDisplay = document.getElementById('statsDisplay');
+let currentHero = hero;
+
+function switchHero(){
+    if(currentHero === hero){
+        currentHero = hero2;
+    } else {
+        currentHero = hero;
+    }
+    displayStats(currentHero);
+}
+
 function rest(object) {
     object.health = 10;
+    statsDisplay.innerHTML = `${object.name} rested and has ${object.health} health again.`;
     return object;
 };
 
 function pickUpItem(whoPicks, pickedWeapon) {
     whoPicks.inventory.push(pickedWeapon);
+    statsDisplay.innerHTML = `${whoPicks.name} gained ${pickedWeapon.type} with ${pickedWeapon.damage} damage.`;
 };
 
 function equipWeapon(whomEquip) {
-    if (!whomEquip.weapon && whomEquip.inventory.lenght > 0) {
-        whomEquip.weapon.type = whomEquip.inventory[0].type;
-        whomEquip.weapon.damage += whomEquip.inventory[0].damage;
-    }
-    console.log(whomEquip.inventory);
-    console.log(whomEquip.weapon);
+    if (whomEquip.inventory.length > 0) {
+        let newWeapon = whomEquip.inventory.shift();
+        whomEquip.weapon = newWeapon;
+    };
+    statsDisplay.innerHTML = `${whomEquip.name} is now equipped with ${whomEquip.weapon.type}.`;
 };
 
-// showing you what I have
+// this is what I got
 
-function displayStats(object){
-    const statsDisplay = document.createElement('p');
-    const imgContainer = document.getElementById('imgContainer');
-    statsDisplay.setAttribute('id', 'statsDisplay');
+function displayStats(object) {
     statsDisplay.innerText = `${object.name} has ${object.health} health and currently carries ${object.weapon.type} with ${object.weapon.damage} damage.`;
-    imgContainer.appendChild(statsDisplay);
+    console.log(statsDisplay);
 }
 
+function toggleFormDisplay() {
+    let computedStyle = window.getComputedStyle(changeNameForm);
+    if (computedStyle.display === "none") {
+        changeNameForm.style.display = "block";
+    } else {
+        changeNameForm.style.display = "none";
+    }
+}
 
-function changeName(object){
+function changeName(object) {
     event.preventDefault();
     const inputField = document.getElementById('name');
     const newName = inputField.value;
     object.name = newName;
     displayStats(object);
+    inputField.value = null;
+    toggleFormDisplay();
 }
+
+function fightsEnemy(whoFights){
+    let damage = Math.floor((Math.random() *10));
+    whoFights.health = whoFights.health - damage;
+    console.log(hero.health);
+    statsDisplay.innerText = `What a fight! ${whoFights.name} lost ${damage} health points!`;
+}
+
